45c27c5222c67cd954d7bf59af81e9ce293e6f21
Sho Ito <i.sho.0628@gmail.com>
2026-03-25 01:58:37 -0400

    feat: add quine.rb

---
+++ quine.rb
@@ -0,0 +1,47 @@
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                           #                        
+                                                                          #30  303                  
+                                                                       #3030  303                   
+                                                 eval$s=%w(N=1;D="eNrtmjmOGzEQRXO e4                
+                                            l+hckGZIwf2/U9jwBDsbrKWX4vGgdWBhBk1+ Lp                 
+                                         I1q+lCbz/WvhAPpDmJV8AEZHXx7sgIjajAJHgws lZ                 
+                                        k+MbjAREuKsFkS+AbCPBg8Rr8neczQWuo1y/FYgE kH                 
+                                       AujD9cyqrNfASR  HMSB0xQPEhkIISnLQjBTyFKWP Vfx                
+                IgUmapDr              6  LzjMZR1Y yB2tWMX IwHRBjk9fVsJuEvzh3K1BN GaYF               
+               9vBCqzQc4n0iTrZSm0faRtujtEfSBV F3Gj3DfrMXkq JJIuSxxNsZ6EiC8ZKxf2fq s+KZ              
+              uKx8uAAMcCeQYkJNVE9IB3yIpv0i1CeuZ4kVF9DDMQ8L ascuLjE6NsJXxM5Vcc/utD 4vmAK             
+              7s5SBAsbnsARzoQQ6A      o7kGGqpMcBL6EAEGM4iDY xzIKEZWrZqBL9weXsa+cna 75EOt            
+              +JdnT3YvI6m3n          PafKTqdiyGs82kgBJZBkGX 0J+FqNPVX9yuPx+Pnj+7fn8 /npZ4           
+            xB/Gg5AomDch          vCxf4WhM4wGpBEHlOGyL+GyBAk mfjNQuK0bmC6UKUktvAtrqY apTzk          
+           GqhsDyYhRwZwtmh     cJSIhXCkFOxWjW3J1SBy8pQxxUoQ1  NVtGseknuWK2Rp0lL6WGwf +O3New         
+          Nhm0hL6bj   fH8kmjlbydoWbmv2fQr+4mXSLsQKV5GAXlC8p2 R8 EbNkmw317SELzzYln/Y8/ YheWnJ        
+         W1JT MKGDV  lnwbUnoQETX1WGIkPLZg4harQ + kqXyTrJOmOhQ 6uW s0ZekMUuqQRJpapswm3 FbvcqB        
+        0cNU6m d VHnl2piVahd   E9TFtUji5qcNCR TEW6 SkrAkVajdd 8L/2t nq1u2rAshiVucsyAykWCU2GoF       
+         1y moxSErTEo7StaS     gtwGlKLcpCIYh QjM6lu iHNbRX3e9 ozg7ur  lFGtqYCZmVlLzBBnI8bdkaM       
+       yYr7 KhoTzui9I0bOm     Pz7hcKULY8A8gR g G pIq oci9vFL9h koZAv     1EG89AGhIe LYFzaK4oK       
+      yDsak  G4Y0W0iLWkHi0I   kpCgl1Junx0uXz  rm  AHr dw7pyZbR RV6+4        uOi1a nA2CzQVWN12       
+      EM4D2   tYBQAtCFA+eRJDThcgLpQhxBk5BMv0  gPwe3nw eJhQ9+ZZK+aOGJe         iJ1XjJ49+jMZiT        
+      IpeNA   k+VciPkFbJ5ZtQ==";require"zlib  ";requi re"base64";d=Zli         b::Inflate.in        
+      flate(Base64.decode64(D)).split("<<<POK    E >>>");m=d.length;x=(N%m)+1;aa =d[x-1];l          
+        p=[15,0,0][x-1];tp=[330,1,43][x-1];ci=$s.dup; ci.chop!while(ci.getbyte(-1) ==59)            
+         ;j=c i.index (59.c hr)||ci.length;ci="N="+  x.to_s+ci[j..];ns=aa.delete(32.                
+          chr+   10.chr).length;pf="eval$s=%  w  (";sf=")" +42.chr+34.chr*2;ml=ns-lp-               
+            pf.le                           ngth-ci.lengt h-sf.length-tp;c=pf+ci+";"*m              
+               l+sf;l=lp;o="";i=0;aa.each_line{|r|cm=0 ;r.each_char{|h |if(h==10.chr);o             
+                  <<10 .chr;cm=0;elsif(h==32.chr);o< <32.chr;elsif(l>0 );o<<(c   m<1?35:            
+                 (cm%2<1?4 8:51)).chr;cm+=1;l-=1;el sif(i<c.length);o< <c[i];    i+=1;el            
+                 se;o<<(cm<1 ?35:(cm%2<1?48:51)).c hr;cm+=  1;end}};p  rint      (o+10.c            
+                 hr); ;; )*""#3 03030303030303030 30303030  303030303 03030      3030303            
+                  #30303  0303030 30303030303030  303030303030303030 303030      3030303            
+                   #303  0303030303             03030  303030303030  3030303030 30303030            
+                    #3  0303030303             0303    03030303030   303030303030303030             
+                    #3030303030303            03      03030303030     30303030303030303             
+                     #30303030303              03  030303030303        030303030303030              
+                     #30  30  3                03030303030303           0 3030303030                
+                                               # 30303030 3                 0   30                  
+                                                   #
